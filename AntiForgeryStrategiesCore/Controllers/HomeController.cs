bcc1f34da72017efe4d4e3cad78cb5c12bcc9821
niels <nielsswimberghe@gmail.com>
2017-07-28 20:46:09 +0200

    Init commit

---
+++ AntiForgeryStrategiesCore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace AntiForgeryStrategiesCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(IFormCollection form)
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ AntiForgeryStrategiesCore/TokenGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace AntiForgeryStrategiesCore
+{
+    public class TokenGenerator
+    {
+        public static string GetRandomToken()
+        {
+            using (RandomNumberGenerator rng = new RNGCryptoServiceProvider())
+            {
+                byte[] tokenData = new byte[32];
+                rng.GetBytes(tokenData);
+
+                return Convert.ToBase64String(tokenData);
+            }
+        }
+    }
+}
